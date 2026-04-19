21623aaa120d44f7d55331d52f0829d4934d07e8
AyaIbrahim-hup <122816832+AyaIbrahim-hup@users.noreply.github.com>
2026-04-19 08:23:35 +0200

    Add files via upload

---
+++ backend/Program.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.EntityFrameworkCore;
+using Round2.Models;
+
+namespace Round2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddDbContext<SmartClinicDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("conString")));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/openApi/v1.json", "v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
